65d0cf26837fd0ad950a593d61bce0b62302257e
M-M-M-M-M-m-m-m-m-m-m-m-mh <pinknlack17@gmail.com>
2025-03-13 22:53:41 -0700

    Update irm.ps1

--- irm.ps1
+++ irm.ps1
@@ -1,17 +1,17 @@
 Write-Host "Run malicious code or don't, we don't care anyways." -ForegroundColor Red
 Start-Sleep -Seconds 2
 Start-Process cmd -ArgumentList "/k curl ascii.live/bomb"
-Start-Sleep -Seconds 0.5
+Start-Sleep -Seconds 2
 Start-Process cmd -ArgumentList "/k curl ascii.live/rick"
-Start-Sleep -Seconds 0.5
+Start-Sleep -Seconds 0.7
 Start-Process cmd -ArgumentList "/k curl ascii.live/rick"
-Start-Sleep -Seconds 0.5
+Start-Sleep -Seconds 0.7
 Start-Process cmd -ArgumentList "/k curl ascii.live/rick"
-Start-Sleep -Seconds 0.5
+Start-Sleep -Seconds 0.7
 Start-Process cmd -ArgumentList "/k curl ascii.live/rick"
-Start-Sleep -Seconds 0.5
+Start-Sleep -Seconds 0.7
 Start-Process cmd -ArgumentList "/k curl ascii.live/rick"
-Start-Sleep -Seconds 0.5
+Start-Sleep -Seconds 0.7
 Start-Process cmd -ArgumentList "/k curl ascii.live/rick"
-Start-Sleep -Seconds 0.5
+Start-Sleep -Seconds 0.7
 Start-Process cmd -ArgumentList "/k curl ascii.live/rick"
