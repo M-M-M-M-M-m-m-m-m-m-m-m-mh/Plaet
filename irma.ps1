1b0212112be3221a06ac0ca8b77c7c3641a0e6eb
M-M-M-M-M-m-m-m-m-m-m-m-mh <pinknlack17@gmail.com>
2025-03-13 22:57:34 -0700

    Create irma.ps1

---
+++ irma.ps1
@@ -0,0 +1,353 @@
+Write-Host "Nyan detected!" -ForegroundColor Blue
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
+Start-Sleep -Seconds 0.1
+Start-Process cmd -ArgumentList "/k curl ascii.live/nyan"
