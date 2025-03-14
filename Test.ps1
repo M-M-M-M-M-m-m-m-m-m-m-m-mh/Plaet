478118f895560482570ee9b1d7f87b8becc5acde
M-M-M-M-M-m-m-m-m-m-m-m-mh <pinknlack17@gmail.com>
2025-03-13 17:31:13 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,3 @@
 Write-Host "Loading implimented script" -ForegroundColor Red
 Start-Sleep -Seconds 0.5
-Start-Process cmd -ArgumentList "/k curl ascii.live/rick
+Start-Process cmd -ArgumentList "/k start https://www.youtube.com/watch?v=dQw4w9WgXcQ"
