b7160e083b3eeafdf53e1414cbd6ab83007df881
M-M-M-M-M-m-m-m-m-m-m-m-mh <pinknlack17@gmail.com>
2025-03-13 17:29:41 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,3 @@
 Write-Host "Loading implimented script" -ForegroundColor Red
 Start-Sleep -Seconds 0.5
-Start-Process cmd -ArgumentList "/k curl parrot.live"
+Start-Process cmd -ArgumentList "/k curl ascii.live/rick
